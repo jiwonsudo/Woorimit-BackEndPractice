52652dd329b8ec5e081da18c7484edf712a2b141
jiwonsudo <wondev112@gmail.com>
2022-03-24 23:46:51 +0900

    서버에서 로그인 인증 기능 만들기

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -16,7 +16,9 @@ function login() {
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
-    });
+        body: JSON.stringify(req),
+    })
+        .then((res) => res.json())
+        .then(console.log);
 }
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const users = {
+  id: ["jiwon", "jinyang", "hyenyang"],
+  pw: ["jiwon50", "jinyang50", "hyenyang50"],
+};
+
 const output = {
     home : (req, res) => {
         res.render("home/index");
@@ -11,7 +16,22 @@ const output = {
 
 const process = {
     login : (req, res) => {
-     console.log(req.body);
+        const id = req.body.id,
+            pw = req.body.pw;
+
+        if (users.id.includes(id)) {
+            const id_index = users.id.indexOf(id);
+            if (users.pw[id_index] === pw) {
+              return res.json({
+                  success : true,
+              });
+            }
+        }
+
+        return res.json({
+            success : false,
+            msg : "Failed to login.",
+        });
     },
 };
 
